5be8621f452c6053eabe7db42daf9505155b026c
Timo Yan <tomyan13@gmail.com>
2019-10-14 20:24:51 +0800

    Add console client code.

--- src/console-client/Program.cs
+++ src/console-client/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
-namespace console_client
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace console_client {
+    public class Program {
+        private static async Task Main () {
+            // discover endpoints from metadata
+            var client = new HttpClient ();
+
+            var disco = await client.GetDiscoveryDocumentAsync ("http://localhost:5000");
+            if (disco.IsError) {
+                Console.WriteLine (disco.Error);
+                return;
+            }
+            Console.WriteLine (disco.Json);
+            
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.AccessToken);
         }
     }
 }
